6a5a2c0ed46ff55fada5b56654d868fe89e6c163
mikeruk <90135195+mikeruk@users.noreply.github.com>
2024-10-13 10:43:09 +0200

    initial commit

---
+++ src/main/java/demo/mybatiscodegendatabasefirstapproach1/MyBatisCodegenDatabaseFirstApproach1Application.java
@@ -0,0 +1,13 @@
+package demo.mybatiscodegendatabasefirstapproach1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisCodegenDatabaseFirstApproach1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisCodegenDatabaseFirstApproach1Application.class, args);
+    }
+
+}
---
+++ src/test/java/demo/mybatiscodegendatabasefirstapproach1/MyBatisCodegenDatabaseFirstApproach1ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.mybatiscodegendatabasefirstapproach1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisCodegenDatabaseFirstApproach1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
